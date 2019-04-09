6cb0aa2dec62a0061d46359f7f94845b444a6823
sunset wan <sunsetwan@gmail.com>
2019-04-09 15:40:23 +0800

    no message

--- DrawPlayingCard/ViewController.swift
+++ DrawPlayingCard/ViewController.swift
@@ -12,21 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var cards = [PlayingCard]()
+        for _ in 1...(cardViews.count / 2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        for cardView in cardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: cards.count.arc4random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+//            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+        }
     }
     
     var deck = PlayingCardDeck()
 
     
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.right, .left]
-            playingCardView.addGestureRecognizer(swipe)
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizeBy:)))
-            playingCardView.addGestureRecognizer(pinch)
-        }
-    }
+    @IBOutlet var cardViews: [PlayingCardView]!
+    
+    
+    
+    
+//    @IBOutlet weak var playingCardView: PlayingCardView! {
+//        didSet {
+//            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+//            swipe.direction = [.right, .left]
+//            playingCardView.addGestureRecognizer(swipe)
+//            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizeBy:)))
+//            playingCardView.addGestureRecognizer(pinch)
+//        }
+//    }
     
     @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
         switch sender.state {
