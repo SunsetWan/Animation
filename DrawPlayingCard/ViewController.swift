66cbe27867c0fc3aed8465ac21549b228e602738
sunset wan <sunsetwan@gmail.com>
2019-04-09 10:32:48 +0800

    repo initialization

---
+++ DrawPlayingCard/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  DrawPlayingCard
+//
+//  Created by sunsetwan on 2019/3/29.
+//  Copyright © 2019 sunsetwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    var deck = PlayingCardDeck()
+
+    
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.right, .left]
+            playingCardView.addGestureRecognizer(swipe)
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizeBy:)))
+            playingCardView.addGestureRecognizer(pinch)
+        }
+    }
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default:
+            break
+        }
+    }
+    
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
+    
+
+}
+
